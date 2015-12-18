6ba6b28b7905b18a02bb58882825e32da508d2e2
AndyM <andym@whatcomtrans.net>
2015-12-18 10:58:20 -0800

    IP tweak to localhost

--- testing/testing.js
+++ testing/testing.js
@@ -1,10 +1,5 @@
 var news = [
-   "Borussia Dortmund wins German championship",
-   "Tornado warning for the Bay Area",
-   "More rain for the weekend",
-   "Android tablets take over the world",
-   "iPad2 sold out",
-   "Nation's rappers down to last two samples"
+﻿
 ];
 
 var dgram = require('dgram'); 
@@ -12,13 +7,13 @@ var server = dgram.createSocket("udp4");
 server.bind();
 server.setBroadcast(true)
 server.setMulticastTTL(128);
-server.addMembership('10.0.1.247'); 
+server.addMembership('127.0.0.1'); 
 
 setInterval(broadcastNew, 3000);
 
 function broadcastNew() {
     var message = new Buffer(news[Math.floor(Math.random()*news.length)]);
-    server.send(message, 0, message.length, 8088, "230.185.192.108");
+    server.send(message, 0, message.length, 8088, "127.0.0.1");
     console.log("Sent " + message + " to the wire...");
     //server.close();
 }
