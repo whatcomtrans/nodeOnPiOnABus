b70864cfbe75fc124dd66f33876aefb6edaf86fd
AndyM <andym@whatcomtrans.net>
2015-12-28 16:01:22 -0800

    path testing

--- myconfig.js
+++ myconfig.js
@@ -1,6 +1,6 @@
 module.exports.myconfig = {
-  "executable": "./production.js",
-  "queuePath": "/queue/",
+  "executable": "/home/pi/nodeOnPiOnABus/production.js",
+  "queuePath": "/home/pi/nodeOnPiOnABus/queue",
   "IoTConfig": {
     "host": "A6R7AFY5KV5WL.iot.us-west-2.amazonaws.com",
     "port": 8883,
@@ -8,8 +8,8 @@ module.exports.myconfig = {
 	  "clientId": "vehicle900",
 	  "thingName": "vehicle900",
 	  "caCert": "root-CA.crt",
-	  "clientCert": "./b853ff8dd6-certificate.pem.crt",
-	  "privateKey": "./b853ff8dd6-private.pem.key"
+	  "clientCert": "b853ff8dd6-certificate.pem.crt",
+	  "privateKey": "b853ff8dd6-private.pem.key"
   },
   "myIP": "192.168.1.15",
   "GPSudpPort": 5067,
