880b949f9b9526a0f2eee1852d95791899318c17
joshn-ridewta-com <joshn@ridewta.com>
2016-09-27 21:52:35 -0700

    typo

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ function checkGitVersion() {
                                    }
                               });
                          });
-                    };
+                    });
                }
           }
      });
