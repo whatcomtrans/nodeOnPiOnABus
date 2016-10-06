2fb09d3592c73dc1550e060fe5f9996ef4f3bba7
joshn-ridewta-com <joshn@ridewta.com>
2016-10-06 14:55:23 -0700

    Ok, ready for production but the Gen 5 DVRs are still not working.

--- index.js
+++ index.js
@@ -85,21 +85,24 @@ function onAwsThing() {
      });
 
      // Update IoT with message string TODO
-     debugConsole("Creating connection to DVR...");
-     tcpDVR = net.createConnection(5067, "192.168.1.129", function(){
-          debugConsole("Connection established to DVR");
-          tcpDVR.on("close", function(had_error){
-               if (had_error) {
-                    debugConsole("Connection to DVR closed due to error.")
-               } else {
-                    debugConsole("Connection to DVR closed.")
-               }
+     var newDVRs = ["831", "832", "833", "834", "835", "836", "837"];
+     if (newDVRs.indexOf(awsThing.getProperty("vehicleId")) > -1 ) {
+          debugConsole("Creating connection to DVR...");
+          tcpDVR = net.createConnection(5067, "192.168.1.129", function(){
+               debugConsole("Connection established to DVR");
+               tcpDVR.on("close", function(had_error){
+                    if (had_error) {
+                         debugConsole("Connection to DVR closed due to error.")
+                    } else {
+                         debugConsole("Connection to DVR closed.")
+                    }
+               });
+               awsThing.on("GPS.GPRMC.message", function(msg){
+                    debugConsole("Sending data to DVR");
+                    sendToDVR(msg);
+               });
           });
-          awsThing.on("GPS.GPRMC.message", function(msg){
-               debugConsole("Sending data to DVR");
-               sendToDVR(msg);
-          });
-     });
+     }
 
      // Update IoT with message string TODO
      awsThing.on("GPS.GPRMC.message", function(msg){
