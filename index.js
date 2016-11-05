4bea194b72482bf3601f6fd582026ff6f0449d4e
joshn-ridewta-com <joshn@ridewta.com>
2016-11-04 19:09:52 -0700

    Renames awsThing as piThing fixes #5

--- index.js
+++ index.js
@@ -36,12 +36,12 @@ var doCheckGitVersion = true;
 var debugOutput = enums.debugOutput.consoleOnly;  // Adjustable by settings.  consoleOnly | mqttOnly | consoleMqtt | none
 var debugLevel = enums.debugLevel.DEBUG;  // Adjustable by settings.  See enums above.
 
-// Track status of awsThing connection
+// Track status of piThing connection
 var connected = false;
 
 // IoT variables
 var awsClient;
-var awsThing;
+var piThing;
 var tcpDVR = null;
 var updGPS = null;
 
@@ -60,43 +60,43 @@ debugConsole("Initial settings: " + JSON.stringify(settings), enums.debugLevel.I
 
 // This function is the essense of the rest of the program.
 // It runs once the thing is created.  Setup all of the ON listeners here.
-function onAwsThing() {
-     debugConsole("Thing created, running onAwsThing");
+function onpiThing() {
+     debugConsole("Thing created, running onpiThing");
 
      // Listen for GPS
-     updGPS = listenForGPS(awsThing.getProperty("GPSudpPort"), awsThing);
+     updGPS = listenForGPS(piThing.getProperty("GPSudpPort"), piThing);
 
      // Verify we are up to date
      checkGitVersion();
 
      // Listen for mqttCommands
-     awsThing.subscribe("/vehicles/" + awsThing.thingName + "/commands", {"qos": 0}, function(err, granted) {
+     piThing.subscribe("/vehicles/" + piThing.thingName + "/commands", {"qos": 0}, function(err, granted) {
           if (err) {
                debugConsole("Error subscribing: " + err);
           } else {
                granted.forEach(function (grantedObj) {
                     debugConsole("Subscribed to " + grantedObj.topic + " with qos " + grantedObj.qos);
                });
-               awsThing.on("message", function(topic, message) {
+               piThing.on("message", function(topic, message) {
                     debugConsole("Recieved on topic " + topic + " message: " + message);
-                    if (topic == "/vehicles/" + awsThing.thingName + "/commands") {
+                    if (topic == "/vehicles/" + piThing.thingName + "/commands") {
                          mqttCommands(message);
                     }
                });
           }
      });
 
-     awsThing.on("GPS.RLN.message", function(msgString) {
+     piThing.on("GPS.RLN.message", function(msgString) {
           awsClient.publish("/vehicles/GPS.RLN.message", msgString);
      });
 
-     awsThing.once("GPS.RLN.message", function(msgString) {
+     piThing.once("GPS.RLN.message", function(msgString) {
           var id = msgString.substr((msgString.indexOf(";ID=")+5),3);
           // is vehicleID different then current settings
-          if (id != awsThing.getProperty("vehicleId")) {
-               debugConsole("Updating vehicleId from " + awsThing.getProperty("vehicleId") + " to " + id);
-               awsThing.reportProperty("vehicleId", id, false, function() {
-                    awsThing.retrieveState(function () {
+          if (id != piThing.getProperty("vehicleId")) {
+               debugConsole("Updating vehicleId from " + piThing.getProperty("vehicleId") + " to " + id);
+               piThing.reportProperty("vehicleId", id, false, function() {
+                    piThing.retrieveState(function () {
                          writeSettings(true);
                     });
                });
@@ -104,23 +104,23 @@ function onAwsThing() {
      });
 
      // If commit is different, run the checkGitVersion
-     awsThing.on("delta", function(state) {
-          if (awsThing.getDeltaProperty("commit") != null) {
+     piThing.on("delta", function(state) {
+          if (piThing.getDeltaProperty("commit") != null) {
                checkGitVersion();
           }
-          if (awsThing.getDeltaProperty("debugOutput") != null) {
-               debugConsole("Changing debugOutput to: " + awsThing.getDeltaProperty("debugOutput"), enums.debugLevel.INFO);
-               setDebugOutput(awsThing.getDeltaProperty("debugOutput"));
+          if (piThing.getDeltaProperty("debugOutput") != null) {
+               debugConsole("Changing debugOutput to: " + piThing.getDeltaProperty("debugOutput"), enums.debugLevel.INFO);
+               setDebugOutput(piThing.getDeltaProperty("debugOutput"));
           }
-          if (awsThing.getDeltaProperty("debugLevel") != null) {
-               debugConsole("Changing debugLevel to: " + awsThing.getDeltaProperty("debugLevel"), enums.debugLevel.INFO);
-               setDebugLevel(awsThing.getDeltaProperty("debugLevel"));
+          if (piThing.getDeltaProperty("debugLevel") != null) {
+               debugConsole("Changing debugLevel to: " + piThing.getDeltaProperty("debugLevel"), enums.debugLevel.INFO);
+               setDebugLevel(piThing.getDeltaProperty("debugLevel"));
           }
      });
 
      // Update IoT with message string TODO
      var newDVRs = ["831", "832", "833", "834", "835", "836", "837"];
-     if (newDVRs.indexOf(awsThing.getProperty("vehicleId")) > -1 ) {
+     if (newDVRs.indexOf(piThing.getProperty("vehicleId")) > -1 ) {
           debugConsole("Creating connection to DVR...");
           tcpDVR = net.createConnection(5070, "192.168.1.129", function(){
                debugConsole("Connection established to DVR");
@@ -131,51 +131,51 @@ function onAwsThing() {
                          debugConsole("Connection to DVR closed.")
                     }
                });
-               awsThing.on("GPS.GPRMC.message", function(msg){
+               piThing.on("GPS.GPRMC.message", function(msg){
                     debugConsole("Sending data to DVR: " + msg);
-                    awsThing.reportProperty("dvrGPSmsg", msg, true);
+                    piThing.reportProperty("dvrGPSmsg", msg, true);
                     sendToDVR(msg);
                });
           });
      }
 
      // Update with GPS info
-     awsThing.on("GPS.GPRMC",function(sentence){
+     piThing.on("GPS.GPRMC",function(sentence){
           debugConsole("Updating lat/lon");
-          awsThing.reportProperty("lat", sentence.lat, true);
-          awsThing.reportProperty("lon", sentence.lon, false);
+          piThing.reportProperty("lat", sentence.lat, true);
+          piThing.reportProperty("lon", sentence.lon, false);
      });
 
      // GPS
-     awsThing.on("GPS.message", function(message) {
+     piThing.on("GPS.message", function(message) {
           var msgString = message.message;
           debugConsole("Recieved GPS message: " + msgString);
           if (msgString.indexOf("$GPRMC") > -1) {
                var sentence = nmea.parse(msgString);
                //sentence.lat = sentence.lat.substring(0,2) + '.' + (sentence.lat.substring(2)/60).toString().replace('.','');
                //sentence.lon = '-' + sentence.lon.substring(0,3) + '.' + (sentence.lon.substring(3)/60).toString().replace('.','');
-               awsThing.emit("GPS.GPRMC.message", msgString);
-               awsThing.emit("GPS.GPRMC",sentence);
+               piThing.emit("GPS.GPRMC.message", msgString);
+               piThing.emit("GPS.GPRMC",sentence);
           }
           if (msgString.indexOf("$GPGSV") > -1) {
                var sentence = nmea.parse(msgString);
-               awsThing.emit("GPS.GPGSV.message", msgString);
-               awsThing.emit("GPS.GPGSV",sentence);
+               piThing.emit("GPS.GPGSV.message", msgString);
+               piThing.emit("GPS.GPGSV",sentence);
           }
           if (msgString.indexOf("$GPGLL") > -1) {
                var sentence = nmea.parse(msgString);
-               awsThing.emit("GPS.GPGLL.message", msgString);
-               awsThing.emit("GPS.GPGLL",sentence);
+               piThing.emit("GPS.GPGLL.message", msgString);
+               piThing.emit("GPS.GPGLL",sentence);
           }
           if (msgString.indexOf("$GPGGA") > -1) {
                var sentence = nmea.parse(msgString);
                //sentence.lat = sentence.lat.substring(0,2) + '.' + (sentence.lat.substring(2)/60).toString().replace('.','');
                //sentence.lon = '-' + sentence.lon.substring(0,3) + '.' + (sentence.lon.substring(3)/60).toString().replace('.','');
-               awsThing.emit("GPS.GPGGA.message", msgString);
-               awsThing.emit("GPS.GPGGA",sentence);
+               piThing.emit("GPS.GPGGA.message", msgString);
+               piThing.emit("GPS.GPGGA",sentence);
           }
           if (msgString.indexOf(">RLN") > -1) {
-               awsThing.emit("GPS.RLN.message",msgString);
+               piThing.emit("GPS.RLN.message",msgString);
           }
           //SAMPLE RLN MESSAGE:
           //>RLN81160000+487864486-1224486923+000180380019+0000174204083E103516402728000000000012;ID=B832;*43<
@@ -195,35 +195,35 @@ function createThing() {
                awsClient = client;
 
                var thingName = "vehicle" + settings.vehicleId;
-               // Create awsThing
+               // Create piThing
                awsClient.thingFactory(thingName, {"persistentSubscribe": true}, false, function(err, thing) {
-                    awsThing = thing;
-                    commands.awsThing = awsThing;
+                    piThing = thing;
+                    commands.piThing = piThing;
                     if (err) {
                          debugConsole("Error: " + err);
                     }
                     debugConsole(JSON.stringify(thing));
                     debugConsole("thing created");
                     // Handle connection status changes
-                    awsThing.on("connect", function() {
+                    piThing.on("connect", function() {
                          connected = true;
                     });
-                    awsThing.on("close", function() {
+                    piThing.on("close", function() {
                          connected = false;
                     });
-                    awsThing.on("offline", function() {
+                    piThing.on("offline", function() {
                          connected = false;
                     });
-                    awsThing.register(function() {
+                    piThing.register(function() {
                          debugConsole("thing registered");
                          connected = true;
-                         awsThing.retrieveState(function(){
+                         piThing.retrieveState(function(){
                               var propName;
                               for (propName in settings) {
-                                   awsThing.reportProperty(propName, settings[propName], true);
+                                   piThing.reportProperty(propName, settings[propName], true);
                               }
-                              awsThing.reportState();
-                              onAwsThing();
+                              piThing.reportState();
+                              onpiThing();
                          });
                     });
                });
@@ -239,13 +239,13 @@ function checkGitVersion() {
                     console.error(`exec error: ${error}`);
                } else {
                     debugConsole("Current git commit is: " + stdout.replace(/(\r\n|\n|\r)/gm,""), enums.debugLevel.INFO);
-                    awsThing.reportProperty("commit", stdout.replace(/(\r\n|\n|\r)/gm,""));
+                    piThing.reportProperty("commit", stdout.replace(/(\r\n|\n|\r)/gm,""));
 
                     // Get notified of the version delta
-                    debugConsole("Delta git commit is: " + awsThing.getDeltaProperty("commit"), enums.debugLevel.INFO);
-                    if (awsThing.getDeltaProperty("commit") != null) {
+                    debugConsole("Delta git commit is: " + piThing.getDeltaProperty("commit"), enums.debugLevel.INFO);
+                    if (piThing.getDeltaProperty("commit") != null) {
                          //change commit
-                         var newCommit = awsThing.getDeltaProperty("commit")
+                         var newCommit = piThing.getDeltaProperty("commit")
                          debugConsole ("Need to update the commit to: " + newCommit, enums.debugLevel.INFO);
                          // Call 'git fetch --all'
                          exec('git fetch --all', (error, stdout, stderr) => {
@@ -261,7 +261,7 @@ function checkGitVersion() {
                                         if (error) {
                                              console.error(`exec error: ${error}`);
                                         } else {
-                                             awsThing.reportProperty("commit", stdout.replace(/(\r\n|\n|\r)/gm,""), false, function() {gracefullExit();});
+                                             piThing.reportProperty("commit", stdout.replace(/(\r\n|\n|\r)/gm,""), false, function() {gracefullExit();});
                                         }
                                    });
                               });
@@ -282,7 +282,7 @@ function writeSettings(restart) {
      if (restart === undefined) {
         restart = false;
     }
-     jsonfile.writeFile("../settings/settings.json", awsThing.getReported(), function (err) {
+     jsonfile.writeFile("../settings/settings.json", piThing.getReported(), function (err) {
           if (err) {
                console.error(err);
           } else {
@@ -302,8 +302,8 @@ function gracefullExit() {
      if (tcpDVR != null) {
           tcpDVR.end();
      }
-     // Disconnect awsThing
-     awsThing.end(false, function() {
+     // Disconnect piThing
+     piThing.end(false, function() {
           process.exit(0);
      });
 }
@@ -340,7 +340,7 @@ process.on("beforeExit", function() {
 
 function mqttConsole(msg) {
      if (connected) {
-          awsThing.publish("/vehicles/" + awsThing.thingName + "/console", msg);
+          piThing.publish("/vehicles/" + piThing.thingName + "/console", msg);
      }
 }
 commands.mqttConsole = mqttConsole;
@@ -404,8 +404,8 @@ commands.debugConsole = debugConsole;
 
 function setDebugOutput(method) {
      debugOutput = method;
-     awsThing.reportProperty("debugOutput", debugOutput, false, function() {
-          awsThing.retrieveState(function () {
+     piThing.reportProperty("debugOutput", debugOutput, false, function() {
+          piThing.retrieveState(function () {
                writeSettings(false);
           });
      });
@@ -414,8 +414,8 @@ commands.setDebugOutput = setDebugOutput;
 
 function setDebugLevel(level) {
      debugLevel = level;
-     awsThing.reportProperty("debugLevel", debugLevel, false, function() {
-          awsThing.retrieveState(function () {
+     piThing.reportProperty("debugLevel", debugLevel, false, function() {
+          piThing.retrieveState(function () {
                writeSettings(false);
           });
      });
