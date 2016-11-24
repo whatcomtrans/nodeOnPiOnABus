c9e1d1b65eb398ca0694539587bad36a6d0e71b1
joshn-ridewta-com <joshn@ridewta.com>
2016-11-23 19:50:52 -0800

    more debuging

--- index.js
+++ index.js
@@ -360,7 +360,7 @@ if (runLevel >= 30) {  // AWS IoT thing representing this Pi
 if (runLevel >= 41) {  // Rudementary GPS to DVR over TCP setup
      var tcpDVR = null;
      var newDVRs = ["831", "832", "833", "834", "835", "836", "837"];
-     if (newDVRs.indexOf(piThing.getProperty("vehicleId")) > -1 ) {
+     if (newDVRs.indexOf(settings.getProperty("vehicleId")) > -1 ) {
           listenerRelay.on("PROCESS.shutdown", function() {
                if (tcpDVR != null) {
                     debugConsole.log("Exiting... Stopping tcpDVR stream", debugConsole.INFO);
@@ -386,7 +386,7 @@ if (runLevel >= 41) {  // Rudementary GPS to DVR over TCP setup
                listenerRelay.on("GPS.RMC", function(data){
                     var msg = data.raw;
                     debugConsole.log("Sending data to DVR: " + msg);
-                    piThing.reportProperty("dvrGPSmsg", msg, true);
+                    //piThing.reportProperty("dvrGPSmsg", msg, true);
                     sendToDVR(msg);
                });
           });
