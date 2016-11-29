5d2f53d05930c9f382726d53b2c7bbeca0e76878
joshn-ridewta-com <joshn@ridewta.com>
2016-11-28 23:37:12 -0800

    more debuging

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ if (runLevel >= 10) {   // GPS lisener
           if (piThing.getDeltaProperty("sourceGPS") != null) {
                Object.assign(gpsSettings, piThing.getDeltaProperty("sourceGPS"));
                debugConsole.log("Changing sourceGPS settings to: " + JSON.stringify(gpsSettings), debugConsole.INFO);
-               piThing.reportProperty("sourceGPS", gpsSettings, true);
+               piThing.reportProperty("sourceGPS", gpsSettings, false);
           }
      });
      listenerRelay.on("piThing.sourceGPSChanged", function() {
