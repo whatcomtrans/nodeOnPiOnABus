029245e8fe219c2b79a81b559481b978021be9a3
joshn-ridewta-com <joshn@ridewta.com>
2016-12-31 11:12:59 -0800

    Had to disable farebox and DVR until further testing is completed.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var doCheckGitVersion = true;
 
 // A run level allows us to quickly enable/disable sections of the script below
 // Each section should be ordered based on dependencies.
-var runLevel = 1000;
+var runLevel = 39;  // TODO Disabled farebox and DVR until further testing is completed.
 
 // Requires
 const awsIoTThing = require("awsiotthing");
